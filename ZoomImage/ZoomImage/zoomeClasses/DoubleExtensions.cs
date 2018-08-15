51b9c5510c1204d6bb9e5c41c52735adb394f565
yasserahmed2999 <yasserahmed2999@yahoo.com>
2018-08-15 14:58:04 +0200

    Add project files.

---
+++ ZoomImage/ZoomImage/zoomeClasses/DoubleExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZoomImage.zoomeClasses
+{
+    class DoubleExtensions
+    {
+        public static double clamp(double self, double min, double max) {
+
+
+            return Math.Min(max, Math.Min(self, min));
+        }
+    }
+}
